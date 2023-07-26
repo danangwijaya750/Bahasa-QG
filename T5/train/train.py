795941118d52f79d9b326ac33f0c451ab637ff33
Danang Wijaya <danangwijaya750@gmail.com>
2023-07-26 17:17:37 +0800

    initial commit

---
+++ T5/train/train.py
@@ -0,0 +1,66 @@
+import argparse
+import datasets
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+from datasets import load_dataset
+
+from dataset import QGDataset
+from trainer import Trainer
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataloader_workers", type=int, default=2)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--epochs", type=int, default=20)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--max_length", type=int, default=64)
+    parser.add_argument("--qg_model", type=str, default="t5-base")
+    parser.add_argument("--pad_mask_id", type=int, default=-100)
+    parser.add_argument("--pin_memory", dest="pin_memory", action="store_true", default=False)
+    parser.add_argument("--save_dir", type=str, default="../models")
+    parser.add_argument("--train_batch_size", type=int, default=4)
+    parser.add_argument("--valid_batch_size", type=int, default=8)
+    return parser.parse_args()
+
+
+def get_tokenizer(checkpoint: str) -> T5Tokenizer:
+    tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+    tokenizer.add_special_tokens(
+        {'additional_special_tokens': ['<answer>', '<context>']}
+    )
+    return tokenizer
+
+
+def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForConditionalGeneration:
+    config = T5Config(decoder_start_token_id=tokenizer.pad_token_id)
+    model = T5ForConditionalGeneration(config).from_pretrained(checkpoint)
+    model.resize_token_embeddings(len(tokenizer))
+    # model = model.to(device)
+    return model
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    tokenizer = get_tokenizer(args.qg_model)
+    train_dataset = '../dataset/train-dataset-indolem.csv'
+    test_dataset = '../dataset/dev-dataset-indolem.csv'
+    data_files = {'train': train_dataset, 'validation': test_dataset}
+    dataset = load_dataset('csv', data_files=data_files, on_bad_lines='skip', streaming=True)
+    train_set = QGDataset(dataset["train"], args.max_length, args.pad_mask_id, tokenizer)
+    valid_set = QGDataset(dataset["validation"], args.max_length, args.pad_mask_id, tokenizer)
+    model = get_model(args.qg_model, args.device, tokenizer)
+    trainer = Trainer(
+        dataloader_workers=args.dataloader_workers,
+        device="cuda",
+        epochs=args.epochs,
+        learning_rate=args.learning_rate,
+        model=model,
+        pin_memory=args.pin_memory,
+        save_dir=args.save_dir,
+        tokenizer=tokenizer,
+        train_batch_size=args.train_batch_size,
+        train_set=train_set,
+        valid_batch_size=args.valid_batch_size,
+        valid_set=valid_set
+    )
+    trainer.train()
